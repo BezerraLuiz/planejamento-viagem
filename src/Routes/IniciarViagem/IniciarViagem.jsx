9480f18b5e6f96775a44d3477e54b50dc8ddc492
Luiz Antônio Ponciano Costa Bezerra <luiz.bezerra>
2024-08-13 20:19:31 -0300

    Iniciado page iniciar-viagem

--- src/Routes/IniciarViagem/IniciarViagem.jsx
+++ src/Routes/IniciarViagem/IniciarViagem.jsx
@@ -3,44 +3,47 @@ import style from "./IniciarViagem.module.css";
 export default function IniciarViagem() {
   return (
     <>
-      <div id={style.container}>
-        <form id={style.form} action="">
-          <div >
-          <div style={{
-            display: "flex",
-          }}>
-            <div className={style.container_input}>
-              <label htmlFor="">Local da Viagem</label>
-              <input type="text" />
+      <div id={style.container_main}>
+        <div id={style.container}>
+          <form id={style.form} action="">
+            <div>
+              <div
+                style={{
+                  display: "flex",
+                  gap: "100px",
+                  marginBottom: "15px",
+                }}
+              >
+                <div className={style.container_input}>
+                  <label htmlFor="">Local da Viagem</label>
+                  <input type="text" />
+                </div>
+                <div className={style.container_input}>
+                  <label htmlFor="">Local da Hospedagem</label>
+                  <input type="text" />
+                </div>
+              </div>
+              <div
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  gap: "100px",
+                }}
+              >
+                <div className={style.container_input}>
+                  <label htmlFor="">Data Início</label>
+                  <input type="date" />
+                </div>
+                <div className={style.container_input}>
+                  <label htmlFor="">Data Fim</label>
+                  <input type="date" />
+                </div>
+              </div>
             </div>
-            <div className={style.container_input}>
-              <label htmlFor="">Local da Hospedagem</label>
-              <input type="text" />
-            </div>
-          </div>
-          <div style={{
-            display: "flex",
-          }}>
-            <div className={style.container_input}>
-              <label htmlFor="">Data Início</label>
-              <input type="date" />
-            </div>
-            <div className={style.container_input}>
-              <label htmlFor="">Data Fim</label>
-              <input type="date" />
-            </div>
-          </div>
-          </div>
-        </form>
+          </form>
 
-        <h2>Orçamentos</h2>
-        <table>
-          <tr>
-            <th>Valor Viagem</th>
-            <th>Valor Hospedagem</th>
-            <th>Valor Orçamento Durante A Viagem</th>
-          </tr>
-        </table>
+          
+        </div>
       </div>
     </>
   );
