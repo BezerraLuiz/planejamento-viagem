da9f9eeb5fb211638fda6a5b26b043def33d8258
Luiz Antônio Ponciano Costa Bezerra <luiz.bezerra>
2024-08-13 19:51:15 -0300

    Lembrar Usuário

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,23 @@ import PrincipalPage from "./Routes/PrincipalPage/PrincipalPage";
 import { useEffect } from "react";
 
 function App() {
-
   useEffect(() => {
     // Verificar se há e-mail e senha salvos no localStorage.
     // Se tiver, faz o login (salva no sessionStorage), se não, faz nada.
-    
+    if (localStorage.length != 0) {
+      const userLogado = localStorage.getItem("lembrarUsuario");
+      console.log(userLogado);
+
+      sessionStorage.setItem("user", userLogado);
+      console.log("Lembrou Usuário")
+    }
   }, []);
 
   return (
     <>
-      <Navbar/>
+      <Navbar />
 
-      <PrincipalPage/>
+      <PrincipalPage />
 
       <Outlet />
       <Footer />
